c6e9c197db1c6b48e92815aba0890a8f015fed87
Christian Skorobogatow <christiansko@icloud.com>
2022-07-17 17:03:51 +0200

    Initial Commit

---
+++ DownloadWithEscaping/DownloadWithEscapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithEscapingApp.swift
+//  DownloadWithEscaping
+//
+//  Created by Christian Skorobogatow on 17/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithEscapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
